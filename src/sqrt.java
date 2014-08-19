801883e1b5ec29d70a2a787525fd08be7c066dae
thahiratasneem <sheik@Sheiks-MacBook-Pro.local>
2014-08-19 15:51:45 +0530

    Square root

---
+++ src/sqrt.java
@@ -0,0 +1,22 @@
+
+public class sqrt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=2;
+		int b=3;
+//int c=a*b;
+//int d=4*a*b;
+//int e=a*a;
+//int f=c-d+e;
+//int g=a-b;
+//float h=f/c*g;
+//System.out.println("Square Root: \u221A"+ "("+ f+")");
+		float c=((a*b)-(4*a*b)+(a*a))/((a*b)*(a-b));
+System.out.println("Square Root: \u221A"+ "("+ c+")");
+System.out.println(Math.sqrt(c));
+
+
+	}
+
+}
